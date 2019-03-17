b7e06cce602277839b954600a48d6716239b242e
github username <github email address>
2019-03-17 18:30:35 -0400

    cleaned up react components

---
+++ src/MainContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './nba-logo-vector-01.png';
+import './App.css';
+const NBA = require("nba");
+
+class MainContainer extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    )
+  }
+
+
+}
+
+export default MainContainer
