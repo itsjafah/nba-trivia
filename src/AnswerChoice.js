f56e2cafc92d671043c6f1ba147b02df632f8687
github username <github email address>
2019-04-17 10:00:01 -0400

    styled answer buttons

--- src/AnswerChoice.js
+++ src/AnswerChoice.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 const AnswerChoice = ( {answerChoice, incrementQuestionIndex, handleSelectedAnswer, selectedAnswer} ) => {
 
-  console.log("answer choice is", answerChoice, "selectedAnswer is:", selectedAnswer);
-
   const className = () => {
     if (selectedAnswer === null || (selectedAnswer.id !== answerChoice.id && answerChoice.correct === false)) {
       return "answer-choice-button"
+    } else if (selectedAnswer.id === answerChoice.id && selectedAnswer.correct === true) {
+      return "correct-answer-choice-button-selected"
     } else if (selectedAnswer && answerChoice.correct === true) {
       return "correct-answer-choice-button"
     } else if (selectedAnswer.correct === false) {
