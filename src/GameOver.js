de2bd39fd6f25a6d20abd961012525897c544030
github username <github email address>
2019-04-18 11:05:40 -0400

    fixed final score

--- src/GameOver.js
+++ src/GameOver.js
@@ -5,7 +5,7 @@ const GameOver = ({ correctAnswers, handlePlayClick }) => {
   return (
     <div className="game-over-div">
       <div className="game-over-header">
-        You shot {(correctAnswers/5) * 100}%!
+        You shot {(correctAnswers/10) * 100}%!
       </div>
       <div className="play-again-div">
         <button className="play-again-button" onClick={handlePlayClick}>Play Again?</button>
